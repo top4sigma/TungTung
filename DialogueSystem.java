d858e8d904c84992d304f8ca32a47a39561beaf9
top4sigma <1104270@lwsd.org>
2026-09-24 12:55:15 -0700

    Finally done with this tungsten
    This is sigma

--- DialogueSystem.java
+++ DialogueSystem.java
@@ -1,16 +1,26 @@
 public class DialogueSystem{
   public static void main(String[] args){
-    
+    outputText("Those who know");
   }
 
   public static void outputText(String input){
     System.out.println("|---- Triple T chan ---------------------------------------|");
     int length = input.length();
-    int repeats = (int)Math.floor(length / 59);
+    int repeats = (int)Math.ceil(length / 59.0);
     for(int i = 0; i < repeats; i++){
       System.out.print("|");
-      System.out.print(input.substring(i  * 59, (i  * 59) + 59));
-      System.out.print("|");
+      if((length - i * 59) >= 59)
+      {
+        System.out.print(input.substring(i  * 59, (i  * 59) + 59));
+      }
+      else{
+        System.out.print(input.substring(i  * 59, (i  * 59) + (length - i * 59)));
+        for(int j = 0; j < 58- (length - i * 59); j++)
+        {
+          System.out.print(" ");
+        }
+      }  
+      System.out.println("|");
     }
     System.out.println("|----------------------------------------------------------|");
   }
