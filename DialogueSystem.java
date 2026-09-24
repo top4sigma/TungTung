7385524f913f9e5a07d5a88ed7ee77563deab687
top4sigma <1104270@lwsd.org>
2026-09-24 12:39:09 -0700

    Dialogue system start
    Mangos will know when this is done

---
+++ DialogueSystem.java
@@ -0,0 +1,15 @@
+public class DialogueSystem{
+  public static void main(String[] args){
+    
+  }
+
+  public static void outputText(String input){
+    System.out.println("|---- Triple T chan ---------------------------------------|");
+    int length = input.length();
+    int repeats = Math.floor(input / 59);
+    for(int i = 0; i < repeats; i++){
+      System.out.print("|");
+      System.out.print(input.substring)
+    }
+  }
+}
