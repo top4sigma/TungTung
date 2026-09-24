14b31ab9caa18214e4f5e840113edc192ae448c1
top4sigma <1104270@lwsd.org>
2026-09-24 13:09:40 -0700

    More character names support

--- DialogueSystem.java
+++ DialogueSystem.java
@@ -1,10 +1,12 @@
 public class DialogueSystem{
   public static void main(String[] args){
-    outputText("Those who know. I am tung tung tung, I am the sigma of all the ligmas. Those who know about mango mustard.");
+    outputText("Those who know. I am tung tung tung, I am the sigma of all the ligmas. Those who know about mango mustard.", "Triple T chan");
   }
 
-  public static void outputText(String input){
-    System.out.println("╔════ Triple T chan ════════════════════════════════════════╗");
+  public static void outputText(String input, String character){
+    System.out.print("╔════ ");
+    System.out.print(character);
+    System.out.println(" ════════════════════════════════════════╗");
     int length = input.length();
     int repeats = (int)Math.ceil(length / 59.0);
     for(int i = 0; i < repeats; i++){
